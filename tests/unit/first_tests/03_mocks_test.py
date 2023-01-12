e3b5d2be6577090e54225e869c0d15b831d8a2b5
Wilhem Sorek <wilhem.sorek@gmail.com>
2023-01-12 15:48:54 +0100

    Update 03_mocks_test.py

--- tests/unit/first_tests/03_mocks_test.py
+++ tests/unit/first_tests/03_mocks_test.py
@@ -2,7 +2,6 @@ from unittest import TestCase
 from unittest.mock import patch, MagicMock
 import requests
 from requests.exceptions import Timeout
-
 from services.star_wars_movies_service import get_star_war_movies_names
 
 
@@ -15,53 +14,38 @@ class MockResponse:
 
 
 class TestMock(TestCase):
-    def test_get_star_war_movies_names_call_the_right_url(self):
-        response = {
-            'films': []
+    def test_confirm_call_fonction(self):
+        reponseAPI = {
+            'films':[]
         }
-
-        with patch.object(requests, 'get', return_value=MockResponse(response)) as request_mock:
+        with patch.object(requests, 'get', return_value=MockResponse(reponseAPI)) as MockReponseAPI:
             get_star_war_movies_names('1')
+        MockReponseAPI.assert_called_once_with(
+            'https://swapi.py4e.com/api/people/1'
+        )
 
-        request_mock.assert_called_once_with(
-            'https://swapi.py4e.com/api/people/1')
+    def test_get_star_wars_movies_name(self):
+        reponsesAttendues = ['Film1','Film2','Film3','Film6','Film7' ]
 
-    def test_get_star_war_movies_names_return_expected(self):
-        expected_names = ['A New Hope', 'The Empire Strikes Back',
-                          'Return of the Jedi', 'Revenge of the Sith', 'The Force Awakens']
-        responses = {
-            'https://swapi.py4e.com/api/people/1': {
-                'films': [
-                    'https://swapi.py4e.com/api/films/1/',
-                    'https://swapi.py4e.com/api/films/2/',
-                    'https://swapi.py4e.com/api/films/3/',
-                    'https://swapi.py4e.com/api/films/6/',
-                    'https://swapi.py4e.com/api/films/7/',
-                ]
+        reponseAPI = {
+            'https://swapi.py4e.com/api/people/1':
+            {
+            'films':
+                [
+                'https://swapi.py4e.com/api/films/1/',
+                'https://swapi.py4e.com/api/films/2/',
+                'https://swapi.py4e.com/api/films/3/',
+                'https://swapi.py4e.com/api/films/6/',
+                'https://swapi.py4e.com/api/films/7/',
+                ],
             },
-            'https://swapi.py4e.com/api/films/1/': {'title': 'A New Hope'},
-            'https://swapi.py4e.com/api/films/2/': {'title': 'The Empire Strikes Back'},
-            'https://swapi.py4e.com/api/films/3/': {'title': 'Return of the Jedi'},
-            'https://swapi.py4e.com/api/films/6/': {'title': 'Revenge of the Sith'},
-            'https://swapi.py4e.com/api/films/7/': {'title': 'The Force Awakens'},
+            'https://swapi.py4e.com/api/films/1/': {'title' : 'Film1'},
+            'https://swapi.py4e.com/api/films/2/': {'title' : 'Film2'},
+            'https://swapi.py4e.com/api/films/3/': {'title' : 'Film3'},
+            'https://swapi.py4e.com/api/films/6/': {'title' : 'Film6'},
+            'https://swapi.py4e.com/api/films/7/': {'title' : 'Film7'},
         }
-
-        with patch.object(requests, 'get', side_effect=lambda url: MockResponse(responses[url])):
-            result = get_star_war_movies_names('1')
-
-        assert result == expected_names
-
-    def test_get_all_movies_names_when_request_raise_exception(self):
-        request_get_mock = patch.object(requests, 'get').start()
-        request_get_mock.side_effect = Timeout
-
-        with self.assertRaises(Timeout):
-            get_star_war_movies_names('1')
-
-    def test_get_star_war_movies_names_return_empty_films(self):
-        expected_names = []
-
-        with patch.object(requests, 'get', return_value=MockResponse({'films': []})) as request_get_mock:
-            result = get_star_war_movies_names('1')
-
-        assert result == expected_names
+        with patch.object(requests, 'get', side_effect=lambda url: MockResponse(reponseAPI[url])):
+            retour = get_star_war_movies_names('1')
+        
+        assert retour == reponsesAttendues
